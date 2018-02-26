c5a208410bb3419a961c5cf19488fe9eab2611e5
Eric Dauenhauer <eric@ericyd.com>
2018-02-26 10:48:33 -0800

    add parent directories before child directories

--- scripts/add-directories.js
+++ scripts/add-directories.js
@@ -3,18 +3,21 @@
  */
 
 const fs = require('fs');
+const path = require('path');
+const config = require('../config');
 
 const directoriesToAdd = [
-    'screenshots',
     'html',
+    path.dirname(config.screenshots.expected),
+    path.dirname(config.screenshots.actual),
+    path.dirname(config.screenshots.diff),
     config.screenshots.actual,
     config.screenshots.expected,
     config.screenshots.diff
-]
+];
 
 directoriesToAdd.forEach(dir => {
     fs.mkdir(dir, err => {
-        if (err && err.message.indexOf('file already exists') !== -1) return;
-        if (err) console.error(err);
+        if (err && err.message.indexOf('file already exists') === -1) console.error(err);
     });
 });
